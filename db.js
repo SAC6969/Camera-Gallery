578e0ca3ef490c13e40da03b7cb9ab6e65ff401e
sac6969 <sachinvermaa1234@gmail.com>
2021-12-14 00:17:30 +0800

    s

--- db.js
+++ db.js
@@ -15,8 +15,6 @@ openRequest.addEventListener("error",function(e){
 openRequest.addEventListener("upgradeneeded",function(e){
     console.log("upgraded");
     db = openRequest.result;
-
     db.createObjectStore("video",{keyPath : "id"});
     db.createObjectStore("image",{keyPath : "id"});
-
 })
